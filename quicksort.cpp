7d73c432706ad4001d6012c5886e7a0061522bd3
kefir09 <47868521+kefir09@users.noreply.github.com>
2019-03-04 21:26:44 +0300

    Update quicksort.cpp

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,1 +1,58 @@
 
+    int recursiyaleft (int left, int left1){
+        if (left1 < left){
+            left = left1;
+            left1 = result1;
+            return result1;
+        }
+    }
+
+
+
+    int main () 
+
+{
+     int a;
+   
+    cout << " ";
+    cin >> a;
+   
+    int* array = (int*)malloc(sizeof(int) * a);
+     
+    for (int i = 0; i < a; i++) {
+        array[i] = rand() % 120;
+        cout << array[i] << " ";
+    }
+    
+    int pivot = array [9];
+    cout << endl << "oporn element " << pivot;
+    
+    int left;  
+    int right; 
+    int left1;
+    int right1;
+    
+    cout << endl << " left side: ";
+    for (int i = 0; i < a; i++) {
+
+        if (pivot > array[i]) {       
+            left = array[i];
+            cout << " "<< left;
+        }
+    }     
+    
+    cout << endl << " right side: ";
+    for (int i = 0; i < a; i++) {
+        if (pivot < array[i]){
+            right = array[i];
+            cout << " "<< right;
+        }
+    }
+    
+    int result1 = recursiyaleft (left,left1);
+    
+    cout <<" "<< "xoba "<< result1;
+    
+    
+    return 0;
+}
